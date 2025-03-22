9fb2bb75a964dab73909d245fb389dbf70997ed4
Laura <laurainnz2@gmail.com>
2025-03-22 19:35:03 +1300

    3.22

--- src/views/home/personal/InfoView.vue
+++ src/views/home/personal/InfoView.vue
@@ -1,11 +1,24 @@
+<!--Personal info page-->
+<template>
+  <el-form label-width="100px" style="width: 500px;">
+    <el-form-item label="head">
+
+
+    </el-form-item>
+    <el-form-item label="nickname">
+      <el-input placeholder="Enter nickname"></el-input>
+    </el-form-item>
+    <el-form-item  label="username">
+      <!-- Set the default value tom for the username input box and
+      disable it (you can only view it but not change it)-->
+      <el-input value="tom" disabled></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary">Save</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
 <script setup>
 
 </script>
-
-<template>
-
-</template>
-
-<style scoped>
-
-</style>
