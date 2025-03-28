b09897966dd63cb12d1363e87b94b9a3e1c0fdea
Laura <laurainnz2@gmail.com>
2025-03-28 14:17:30 +1300

    3.28

--- baking/src/main/java/cn/tedu/baking/controller/UserController.java
+++ baking/src/main/java/cn/tedu/baking/controller/UserController.java
@@ -11,12 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1/users")
 public class UserController {
-    @Autowired
-    IUserService userService;
+    //@Autowired
+    //IUserService userService;
     @PostMapping("/reg")
     public JsonResult reg(UserRegDTO userRegDTO){
         System.out.println("userRegDTO = " + userRegDTO);
-        userService.reg(userRegDTO);
+        //userService.reg(userRegDTO);
         return JsonResult.ok();
     }
 }
