e6b4a99c3caaf42b574de8e5c0f03a4b6c0c86c0
Laura <laurainnz2@gmail.com>
2025-07-05 19:06:18 +1200

    Modify ListView 7.5.2025

--- baking-web/src/views/home/ListView.vue
+++ baking-web/src/views/home/ListView.vue
@@ -3,16 +3,16 @@
   <div style="width: 1200px;margin: 0 auto;">
     <!--Cope HomeView four el-row -->
     <el-row :gutter="10">
-      <el-col :span="6" v-for="item in 4" style="margin-top: 10px;">
+      <el-col :span="6" v-for="item in contentArr" style="margin-top: 10px;">
         <el-card>
-          <router-link to="/detail" style="text-decoration: none;color:#333;">
-            <img src="/imgs/a.jpg" style="width:100%;height:150px;">
-            <p style="height:40px;">Cream Bread</p>
+          <router-link :to="'/detail?id='+item.id" style="text-decoration: none;color:#333;">
+            <img :src="'http://localhost:8080'+item.imgUrl" style="width:100%;height:150px;">
+            <p style="height:40px;">{{ item.title }}</p>
           </router-link>
           <el-row :gutter="10">
-            <el-col :span="4"><el-avatar src="/imgs/head.jpg"></el-avatar></el-col>
-            <el-col :span="16" style="font-size:20px;line-height:40px;">Michael</el-col>
-            <el-col :Span="4" style="line-height:40px;">欧包</el-col>
+            <el-col :span="4"><el-avatar :src="'http://localhost:8080'+item.userImgUrl"></el-avatar></el-col>
+            <el-col :span="13" style="font-size:20px;line-height:40px;">{{  item.nickname }}</el-col>
+            <el-col :Span="7" style="line-height:40px;">{{ item.categoryName }}</el-col>
           </el-row>
         </el-card>
       </el-col>
@@ -25,5 +25,18 @@
 </style>
 
 <script setup>
+import {onMounted, ref} from "vue";
+import axios from "axios";
 
+const contentArr = ref([]);
+
+onMounted(()=>{
+  let type = new URLSearchParams(location.search).get('type');
+  console.log(type);
+  axios.get('http://localhost:8080/v1/contents/'+type+'/type').then((response)=>{
+    if(response.data.code==2001){
+      contentArr.value = response.data.data;
+    }
+  })
+})
 </script>
