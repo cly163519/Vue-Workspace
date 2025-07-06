c346fb93f47f80d7a8dfd13c5e04d113fb194e1c
Laura <laurainnz2@gmail.com>
2025-07-06 16:08:53 +1200

    Modify BannerView 7.6.2025

--- baking-web/src/views/admin/BannerView.vue
+++ baking-web/src/views/admin/BannerView.vue
@@ -10,7 +10,7 @@
     <el-table-column prop="createTime" label="createTime" align="center"></el-table-column>
     <el-table-column label="operation" align="center">
       <template #default="scope">
-        <el-button type="danger" @click="del(scope.$index)">delete</el-button>
+        <el-button type="danger" @click="del(scope.$index,scope.row)">delete</el-button>
       </template>
     </el-table-column>
   </el-table>
@@ -35,10 +35,14 @@ onMounted(()=>{
     }
   })
 })
-const del = (i) =>{
+const del = (i,banner) => {
   if(confirm('Do you want to delete carousel picture?')){
-    arr.value.splice(i,1);
-    ElMessage.success('删除成功！')
+    axios.post(BASE_URL+'/v1/banners/'+banner.id+'/delete').then((response)=>{
+      if(response.data.code==2001){
+        arr.value.splice(i,1);
+        ElMessage.success('删除成功！')
+      }
+    })
   }
 }
 </script>
